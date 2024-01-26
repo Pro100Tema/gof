825e3286b4f901c5c77eeff2297670f20c68ee60
Pro100Tema <40834728+Pro100Tema@users.noreply.github.com>
2024-01-26 19:37:11 +0300

    Update create_initial_ds.py

--- create_initial_ds.py
+++ create_initial_ds.py
@@ -2,35 +2,72 @@ from ROOT import *
 import random as rd
 import numpy as np 
 
-x = RooRealVar("x", "x", 0, 100)
-y = RooRealVar("y", "y", 0, 100)
 
-# Задаем параметры функции Гаусса
-mean = 0.0
-sigma = 1.0
+def create_mc_data(data):
+    x = RooRealVar("x", "x", -100, 100)
+    y = RooRealVar("y", "y", -100, 100)
 
-# Задаем количество точек в изначальном датасете
-num_points = 100
+    # Задаем параметры функции Гаусса
+    mean = 0.0
+    sigma = 5.0
 
-varset = RooArgSet(x, y)
+    # Задаем количество точек в изначальном датасете
+    num_points = len(data) * 100
 
-dataset = ROOT.RooDataSet("dataset", "dataset", varset)
+    varset = RooArgSet(x, y)
 
-# Генерируем случайные данные для x
-x_val = np.random.normal(mean, sigma, num_points)
+    dataset = ROOT.RooDataSet("dataset_mc", "dataset_mc", varset)
 
-# Генерируем соответствующие значения функции Гаусса
-y_val = np.exp(-((x_val - mean) / sigma)**2 / 2) / (sigma * np.sqrt(2 * np.pi))
+    # Генерируем случайные данные для x
+    x_val = np.random.normal(mean, sigma, num_points)
 
-# Нормализуем значения функции, чтобы их сумма была равна 1
-y_val /= np.sum(y_val)
+    # Генерируем соответствующие значения функции Гаусса
+    y_val = np.exp(-((x_val - mean) / sigma)**2 / 2) / (sigma * np.sqrt(2 * np.pi))
 
-for i in range(num_points):
-    x.setVal(x_val[i])
-    y.setVal(y_val[i])
-    dataset.add(varset)
+    # Нормализуем значения функции, чтобы их сумма была равна 1
+    y_val /= np.sum(y_val)
 
-dataset.SaveAs('dataset.root')
+    for i in range(num_points):
+        x.setVal(x_val[i])
+        y.setVal(y_val[i])
+        dataset.add(varset)
 
-for i in dataset:
-    print(i.x, i.y)
+    dataset.SaveAs('dataset_mc.root')
+    return dataset
+
+
+def create_mc_data(data):
+    x = RooRealVar("x", "x", -100, 100)
+    y = RooRealVar("y", "y", -100, 100)
+
+    # Задаем параметры функции Гаусса
+    mean = 0.0
+    sigma = 5.0
+
+    # Задаем количество точек в изначальном датасете
+    num_points = 100
+
+    varset = RooArgSet(x, y)
+
+    dataset = ROOT.RooDataSet("dataset", "dataset", varset)
+
+    # Генерируем случайные данные для x
+    x_val = np.random.normal(mean, sigma, num_points)
+
+    # Генерируем соответствующие значения функции Гаусса
+    y_val = np.exp(-((x_val - mean) / sigma)**2 / 2) / (sigma * np.sqrt(2 * np.pi))
+
+    # Нормализуем значения функции, чтобы их сумма была равна 1
+    y_val /= np.sum(y_val)
+
+    for i in range(num_points):
+        x.setVal(x_val[i])
+        y.setVal(y_val[i])
+        dataset.add(varset)
+
+    dataset.SaveAs('dataset.root')
+    return dataset
+
+
+#for i in dataset:
+#    print(i.x, i.y)
