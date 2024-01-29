5d3a68cf53f430b59bd3ae4fb748a6d0490b4a73
Pro100Tema <40834728+Pro100Tema@users.noreply.github.com>
2024-01-30 00:48:13 +0300

    Update good_fits.py
    add calculation for each variable in var_lst

--- good_fits.py
+++ good_fits.py
@@ -5,52 +5,57 @@
 #
 # T_value, p_value = good_fits(dataset, dataset_mc, ['x', 'y'])
 # output:
-# T_value and p_value calculated using point-to-point-dissimilarity method
+# T_value and p_value for each variable from var_lst calculated using point-to-point-dissimilarity method
 
 from ROOT import * 
 import numpy as np
 import os
 from   ostap.fitting.ds2numpy  import ds2numpy
 
-#point-to-point-dissimilarity method
-def dissimilarity_method(data, mc_data, var_lst, sigma=0.01, n_permutations=10):
+
+
+def dissimilarity_method(data, mc_data, var_lst, sigma=0.01, n_permutations=100):
     nd = len(data)
     nmc = len(mc_data)
-    x_val = var_lst[0]
 
-    # определение функции psi (из статьи выбор как e^(-x^2) / (2*sigma^2))
-    # значение sigma выбирается из статьи, sigma = 0.01
+    # Определение функции psi (из статьи выбор как e^(-x^2) / (2*sigma^2))
+    # Значение sigma выбирается из статьи, sigma = 0.01
     def psi(x):
         return np.exp(-x**2 / (2 * sigma**2))
 
-    # вычисление значения T-value по формуле из статьи
-    def calculate_T(data, mc_data):
-        term1 = np.sum(psi(np.abs(data[x_val][:, None] - data[x_val])), axis=(0, 1))
-        term2 = np.sum(psi(np.abs(data[x_val][:, None] - mc_data[x_val])), axis=(0, 1))
+    # Вычисление значения T-value по формуле из статьи
+    def calculate_T(data, mc_data, var):
+        term1 = np.sum(psi(np.abs(data[var][:, None] - data[var])), axis=(0, 1))
+        term2 = np.sum(psi(np.abs(data[var][:, None] - mc_data[var])), axis=(0, 1))
         return (1 / (nd**2)) * term1 - (1 / (nd * nmc)) * term2
 
-    observed_T = calculate_T(data, mc_data)
+    #numpy массивы для вычисленных значений T и p-value
+    observed_T_values = np.zeros(len(var_lst))
+    p_values = np.zeros(len(var_lst))
 
-    # реализация перестановочного теста (Permutation test)
-    permuted_T_values = np.zeros(n_permutations)
-
+    # Реализация перестановочного теста (Permutation test) для каждой переменной
     #повторяется n раз, для получения нескольких значений T-value,
     #для получения значения p_value должно выполняться условие T < T_perm
-    for i in range(n_permutations):
-        # комбинация и случайный выбор исходных данных и данных МК
-        combined_data = np.concatenate([data, mc_data])
-        np.random.shuffle(combined_data)
+    for idx, var in enumerate(var_lst):
+        observed_T_values_var = np.zeros(n_permutations)
+        for i in range(n_permutations):
+            # комбинация и случайный выбор исходных данных и данных МК
+            combined_data = np.concatenate([data, mc_data])
+            np.random.shuffle(combined_data)
 
-        permuted_data = combined_data[:nd]
-        permuted_mc_data = combined_data[nd:]
+            permuted_data = combined_data[:nd]
+            permuted_mc_data = combined_data[nd:]
 
+            permuted_T = calculate_T(permuted_data, permuted_mc_data, var)
+            observed_T_values_var[i] = permuted_T
+        
         #вычисление T-value
-        permuted_T_values[i] = calculate_T(permuted_data, permuted_mc_data)
+        observed_T = calculate_T(data, mc_data, var)
+        # выбор p-value
+        p_values[idx] = np.mean(observed_T_values_var >= observed_T)
+        observed_T_values[idx] = observed_T
 
-    # выбор p-value
-    p_value = np.mean(permuted_T_values >= observed_T)
-
-    return observed_T, p_value
+    return observed_T_values, p_values
 
 def good_fits(data, data_mc, var_lst):
 
@@ -58,5 +63,4 @@ def good_fits(data, data_mc, var_lst):
     ds_mc = ds2numpy(data_mc, var_lst)
 
     T_value, p_value = dissimilarity_method(ds, ds_mc, var_lst)
-
     return T_value, p_value
