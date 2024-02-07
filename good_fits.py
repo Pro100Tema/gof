18552c4209b5bf5006b97618bfa8fc8c28b966fb
Pro100Tema <40834728+Pro100Tema@users.noreply.github.com>
2024-02-07 11:19:58 +0300

    Update good_fits.py
    Update pont-to-point to calculate general p-value for all vars

--- good_fits.py
+++ good_fits.py
@@ -16,8 +16,8 @@ from   ostap.fitting.ds2numpy  import ds2numpy
 def distance_to_nearest_neighbor(data):
     from scipy.spatial import cKDTree
 
-    tree = cKDTree(data) # создание k-мерного дерева 
-    distances, _ = tree.query(data, k=2) # расчет расстояния до ближайшего соседа
+    tree = cKDTree(data) # creating k-tree
+    distances, _ = tree.query(data, k=2) # calculating the distance to the nearest neighbor
 
     return distances[:, 1]
 
@@ -26,44 +26,51 @@ def dissimilarity_method(data, mc_data, var_lst, sigma=0.01, n_permutations=100)
     nd = len(data)
     nmc = len(mc_data)
 
-    # Определение функции psi (из статьи выбор как e^(-x^2) / (2*sigma^2))
-    # Значение sigma выбирается из статьи, sigma = 0.01
+    # Definition of the psi function (psi(x) = e^(-x^2) / (2*sigma^2))
+    # The value of sigma is chosen: sigma = 0.01
     def psi(x):
         return np.exp(-x**2 / (2 * sigma**2))
 
-    # Вычисление значения T-value по формуле из статьи
+    # Calculation of the T-value according to the formula from the article
     def calculate_T(data, mc_data, var):
         term1 = np.sum(psi(np.abs(data[var][:, None] - data[var])), axis=(0, 1))
         term2 = np.sum(psi(np.abs(data[var][:, None] - mc_data[var])), axis=(0, 1))
         return (1 / (nd**2)) * term1 - (1 / (nd * nmc)) * term2
 
-    #numpy массивы для вычисленных значений T и p-value
+    # numpy arrays for calculated T-values
     observed_T_values = np.zeros(len(var_lst))
-    p_values = np.zeros(len(var_lst))
-
-    # Реализация перестановочного теста (Permutation test) для каждой переменной
-    #повторяется n раз, для получения нескольких значений T-value,
-    #для получения значения p_value должно выполняться условие T < T_perm
+    
+    # Calculate all T-values
     for idx, var in enumerate(var_lst):
-        observed_T_values_var = np.zeros(n_permutations)
-        for i in range(n_permutations):
-            # комбинация и случайный выбор исходных данных и данных МК
-            combined_data = np.concatenate([data, mc_data])
-            np.random.shuffle(combined_data)
-
-            permuted_data = combined_data[:nd]
-            permuted_mc_data = combined_data[nd:]
-
-            permuted_T = calculate_T(permuted_data, permuted_mc_data, var)
-            observed_T_values_var[i] = permuted_T
-        
-        #вычисление T-value
         observed_T = calculate_T(data, mc_data, var)
-        # выбор p-value
-        p_values[idx] = np.mean(observed_T_values_var >= observed_T)
         observed_T_values[idx] = observed_T
+    
+    # Implementation of the permutation test for each variable
+    # Repeated n times to obtain multiple T-value instances,
+    # to get the p_value, the condition T < T_perm must be satisfied
+    p_values = np.zeros_like(observed_T_values)
+    observed_T_sum = np.sum(observed_T_values)
+    observed_T_values_perm = np.zeros(n_permutations)
 
-    return observed_T_values, p_values
+    # Iterate over a range of values representing the number of permutations
+    for i in range(n_permutations):
+        # Combination and random selection of original data and MC data
+        combined_data = np.concatenate([data, mc_data])
+        np.random.shuffle(combined_data)
+        
+        permuted_data = combined_data[:nd]
+        permuted_mc_data = combined_data[nd:]
+        
+        # Calculation of T-values for permuted data
+        permuted_T_values = np.array([calculate_T(permuted_data, permuted_mc_data, var) for var in var_lst])
+        observed_T_values_perm[i] = np.sum(permuted_T_values)
+    
+    # Calculate p-value as the fraction of cases 
+    # where the sum of T-values for permuted data
+    # is less than the sum of T-values for observed data
+    p_value = np.mean(observed_T_values_perm < observed_T_sum)
+    
+    return observed_T_values, p_value
 
 def good_fits(data, data_mc = [], var_lst = [], method = 'dism'):
 
