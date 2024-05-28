9d03a464aa97144732e4c67a6b002cad951e80f4
Pro100Tema <40834728+Pro100Tema@users.noreply.github.com>
2024-05-28 09:15:35 +0300

    Update good_fits.py
    add new calculation method

--- good_fits.py
+++ good_fits.py
@@ -52,11 +52,9 @@ def distance_to_nearest_neighbor(data):
 
 # Calculation of the T-value according to the formula from the article
 def calculate_T(sum_nd, sum_mc, nd, nmc):
-    #term1 = np.sum(psi_distances_data)
-    #term2 = np.sum(psi_distances_mc_data)
     return (1 / (nd**2)) * sum_nd - (1 / (nd * nmc)) * sum_mc
 
-# Combination and random selection of original data and MC data
+## Combination and random selection of original data and MC data
 def permute_data(data, mc_data):
     combined_data = np.concatenate([data, mc_data])
     np.random.shuffle(combined_data)
@@ -64,136 +62,59 @@ def permute_data(data, mc_data):
     permuted_mc_data = combined_data[len(data):]
     return permuted_data, permuted_mc_data
 
-# old method using cdist
-#def calculate_distance(data1, data2, var_lst):
-#    data1_vars = np.column_stack([data1[var] for var in var_lst])
-#    data2_vars = np.column_stack([data2[var] for var in var_lst])
-#
-#    distances = cdist(data1_vars, data2_vars, 'euclidean').flatten()
-#    return distances
+# calculation ditance between points of datasets
+def calculate_distance_iterative(data1, data2, var_lst):
+    sum_distances = 0
+    for i in range(len(data1)):
+        for j in range(len(data2)):
+            sum_distances += np.linalg.norm([data1[i][var] - data2[j][var] for var in var_lst])
+    return sum_distances
 
-# Calculate distance between each pair of points
-@jit(nopython=True)
-def calculate_block_distance(data_block):
-    n = data_block.shape[0]
-    total_distance = 0.0
-
-    # Iterate over all pairs of points
-    for i in range(n):
-        for j in range(i + 1, n):
-            # Calculate squared Euclidean distance between points
-            # and add square to total distance
-            sum_sq = np.sum((data_block[i] - data_block[j]) ** 2)
-            total_distance += np.sqrt(sum_sq)
-    return total_distance
-
-# Calculate total distance within a dataset
-def calculate_distance_nd(data, var_lst, block_size=1000):
-    data_array = np.column_stack([data[var] for var in var_lst])
-    total_distance = 0
-    num_blocks = (len(data) + block_size - 1) // block_size
-
-    # Iterate over all data blocks
-    for i in range(num_blocks):
-        start = i * block_size
-        end = min((i + 1) * block_size, len(data))
-        data_block = data_array[start:end]
-        total_distance += calculate_block_distance(data_block)
-
-    return total_distance
-
-# Calculate distance between each pair of points from data and mc_data
-@jit(nopython=True)
-def calculate_block_distances(data_block, mc_data_block):
-    n = data_block.shape[0]
-    m = mc_data_block.shape[0]
-    total_distance = 0.0
-    # Iterate over all pairs of points from data and mc_data
-    for i in range(n):
-        for j in range(m):
-            # Calculate squared Euclidean distance between points
-            # and add square to total distance
-            sum_sq = np.sum((data_block[i] - mc_data_block[j]) ** 2)
-            total_distance += np.sqrt(sum_sq)
-    return total_distance
-
-# Calculate total distance between data and mc_data
-def calculate_distance_nmc(data, mc_data, var_lst, block_size=1000):
-    data_array = np.column_stack([data[var] for var in var_lst])
-    mc_data_array = np.column_stack([mc_data[var] for var in var_lst])
-
-    total_distance = 0
-    num_blocks_data = (len(data) + block_size - 1) // block_size
-    num_blocks_mc_data = (len(mc_data) + block_size - 1) // block_size
-    
-    # Iterate over all data blocks
-    for i in range(num_blocks_data):
-        start_i = i * block_size
-        end_i = min((i + 1) * block_size, len(data))
-        data_block = data_array[start_i:end_i]
-
-        for j in range(num_blocks_mc_data):
-            start_j = j * block_size
-            end_j = min((j + 1) * block_size, len(mc_data))
-            mc_data_block = mc_data_array[start_j:end_j]
-
-            total_distance += calculate_block_distances(data_block, mc_data_block)
-
-    return total_distance
-
+# using cdist for calculate distance of large datasets (len > 10000)
+def calculate_distance(data1, data2, var_lst):
+    data1_vars = np.column_stack([data1[var] for var in var_lst])
+    data2_vars = np.column_stack([data2[var] for var in var_lst])
+    distances = cdist(data1_vars, data2_vars, 'euclidean').flatten()
+    return np.sqrt(np.sum(distances))
 
 # Implementation of the permutation test for each variable
 # Repeated n times to obtain multiple T-value instances,
 # to get the p_value, the condition T < T_perm must be satisfied
-def calculate_permuted_T(data, mc_data, var_lst):
+def calculate_permuted_T(data, mc_data, var_lst, large_data=False):
     permuted_data, permuted_mc_data = permute_data(data, mc_data)
-    permuted_distances_data = calculate_distance_nd(permuted_data, var_lst)
-    permuted_distances_mc_data = calculate_distance_nmc(permuted_data, permuted_mc_data, var_lst)
+    if large_data:
+        permuted_distances_data = calculate_distance_iterative(permuted_data, permuted_data, var_lst)
+        permuted_distances_mc_data = calculate_distance_iterative(permuted_data, permuted_mc_data, var_lst)
+    else:
+        permuted_distances_data = calculate_distance(permuted_data, permuted_data, var_lst)
+        permuted_distances_mc_data = calculate_distance(permuted_data, permuted_mc_data, var_lst)
     permuted_T_value = calculate_T(permuted_distances_data, permuted_distances_mc_data, len(permuted_data), len(permuted_mc_data))
     return permuted_T_value
-
 
 def dissimilarity_method(data, mc_data, var_lst, n_permutations=25):
     nd = len(data)
     nmc = len(mc_data)
 
-    # Pre-calculate distances between original data and MC data
-    distances_data = calculate_distance_nd(data, var_lst)
-    distances_mc_data = calculate_distance_nmc(data, mc_data, var_lst)
+    large_data = nd < 10000 or nmc < 10000
 
-    # Pre-calculate psi function values for all distances
-    #psi_distances_data = psi(distances_data)
-    #psi_distances_mc_data = psi(distances_mc_data)
+    if large_data:
+        distances_data = calculate_distance_iterative(data, data, var_lst)
+        distances_mc_data = calculate_distance_iterative(data, mc_data, var_lst)
+    else:
+        distances_data = calculate_distance(data, data, var_lst)
+        distances_mc_data = calculate_distance(data, mc_data, var_lst)
 
     observed_T = calculate_T(distances_data, distances_mc_data, nd, nmc)
-
-
-    #PREVIOUS VERSION
-    #permuted_T_values = np.zeros(n_permutations)
-    #for i in range(n_permutations):
-        #permuted_data, permuted_mc_data = permute_data(data, mc_data)
-        #permuted_distances_data = calculate_distance(permuted_data, permuted_data, var_lst)
-        #permuted_distances_mc_data = calculate_distance(permuted_data, permuted_mc_data, var_lst)
-        #permuted_T_values[i] = calculate_T(permuted_distances_data, permuted_distances_mc_data, nd, nmc)
-        #permuted_T_values[i] = calculate_permuted_T(data, mc_data, var_lst, sigma)
-
-
+    
     # add parallel process to speed up permutation test work 
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        tasks = [executor.submit(calculate_permuted_T, data, mc_data, var_lst) for _ in range(10)]
+        tasks = [executor.submit(calculate_permuted_T, data, mc_data, var_lst, large_data) for _ in range(10)]
         permuted_T_values = np.array([task.result() for task in concurrent.futures.as_completed(tasks)])
 
-    #with concurrent.futures.ThreadPoolExecutor() as executor:
-    #    tasks = [executor.submit(calculate_permuted_T, data, mc_data, var_lst) for _ in range(10)]
-    #    permuted_T_values = np.array([task.result() for task in concurrent.futures.as_completed(tasks)])
-
-
-    # Calculate p-value as the fraction of cases 
-    # where the sum of T-values for permuted data
-    # is less than the sum of T-values for observed data
     p_value = np.mean(permuted_T_values < observed_T)
 
     return observed_T, p_value
+
 
 def good_fits(data, data_mc = [], var_lst = [], method = 'dism'):
 
