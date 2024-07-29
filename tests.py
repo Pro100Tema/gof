218ca36d65a2167cbf439800a825f6808db46901
Pro100Tema <40834728+Pro100Tema@users.noreply.github.com>
2024-07-30 01:24:03 +0300

    Update tests.py
    Add new tests with max memory usage using tracemalloc library

--- tests.py
+++ tests.py
@@ -14,6 +14,8 @@ from   ostap.utils.utils       import vrange
 from   builtins                import range
 import ROOT, time
 from good_fits import good_fits
+import tracemalloc
+from ROOT import TFile
 # =============================================================================
 # logging 
 # =============================================================================
@@ -24,19 +26,48 @@ else :
     logger = getLogger ( __name__ )
 # =============================================================================
 
-def test_point_to_point():
-    f = ROOT.TFile('dataset.root', 'read')
+def perform_test(data_file, mc_file, var_lst, method='PPD'):
+    # Load dataset
+    f = TFile(data_file, 'read')
     dataset = f['dataset']
     f.close()
 
-    f = ROOT.TFile('dataset_mc.root', 'read')
+    # Load MC dataset
+    f = TFile(mc_file, 'read')
     dataset_mc = f['dataset_mc']
     f.close()
 
-    T_value, p_value = good_fits(dataset, dataset_mc, ['x', 'y'], 'dism')
+    # Run good_fits function
+    T_value, p_value = good_fits(dataset, dataset_mc, var_lst, method)
+    return T_value, p_value
 
-    print("Observed Dissimilarity Statistic:", T_value)
-    print("Permutation Test P-value:", p_value)
+def measure_memory_usage(test_func, *args):
+    tracemalloc.start()
+
+    # Run the test function
+    result = test_func(*args)
+
+    # Get current and peak memory usage
+    current, peak = tracemalloc.get_traced_memory()
+    print(f"Current memory usage: {current / 10**6} MB")
+    print(f"Peak memory usage: {peak / 10**6} MB")
+
+    tracemalloc.stop()
+
+    return result
+
+# Test functions
+def test_p2p_cdist():
+    result = measure_memory_usage(perform_test, 'dataset.root', 'dataset_mc.root', ['x', 'y'], 'PPD')
+    print("Permutation Test P-value:", result[1])
+
+def test_p2p_rd_10k():
+    result = measure_memory_usage(perform_test, 'dataset_rd_10k.root', 'dataset_mc.root', ['x', 'y'], 'PPD')
+    print("Permutation Test P-value:", result[1])
+
+def test_p2p_mc_100k():
+    result = measure_memory_usage(perform_test, 'dataset_rd_10k.root', 'dataset_mc_100k.root', ['x', 'y'], 'PPD')
+    print("Permutation Test P-value:", result[1])
 
 def test_kNN():
     f = ROOT.TFile('dataset.root', 'read')
@@ -46,10 +77,60 @@ def test_kNN():
     distance = good_fits(dataset, method = 'kNN')
     print("Distances for dataset:", distance)
 
+def test_LD():
+    f = ROOT.TFile('dataset.root', 'read')
+    dataset = f['dataset']
+    f.close()
+
+    f = TFile('dataset_mc.root', 'read')
+    dataset_mc = f['dataset_mc']
+    f.close()
+
+    U_value, p_value = good_fits(dataset, dataset_mc, ['x', 'y'], method = 'LD')
+    print("Permutation Test P-value:", p_value)
+
+def test_KB():
+    f = ROOT.TFile('dataset.root', 'read')
+    dataset = f['dataset']
+    f.close()
+
+    f = TFile('dataset_mc.root', 'read')
+    dataset_mc = f['dataset_mc']
+    f.close()
+
+    U_value, p_value = good_fits(dataset, dataset_mc, ['x', 'y'], method = 'KB')
+    print("Permutation Test P-value:", p_value)
+
+def test_MS():
+    f = ROOT.TFile('dataset.root', 'read')
+    dataset = f['dataset']
+    f.close()
+
+    f = TFile('dataset_mc.root', 'read')
+    dataset_mc = f['dataset_mc']
+    f.close()
+
+    U_value, p_value = good_fits(dataset, dataset_mc, ['x', 'y'], method = 'MS')
+    print("Permutation Test P-value:", p_value)
+
 if '__main__' == __name__ :
+    with timing ('Test p2p cdist' , logger ) :
+        test_p2p_cdist()
+    
+    with timing ('Test p2p rd 10k' , logger ) :
+        test_p2p_rd_10k()
 
-    with timing ('Test point to point' , logger ) :
-        test_point_to_point()
-
-    with timing ('Test distance to nearest neighbor' , logger ) :
+    with timing ('Test p2p mc 100k' , logger ) :
+        test_p2p_mc_100k()
+    
+    with timing ('Test kNN' , logger ) :
         test_kNN()
+    
+    with timing ('Test LD' , logger ) :
+        test_LD()
+
+    with timing ('Test KB' , logger ) :
+        test_KB()
+    
+    with timing ('Test MS' , logger ) :
+        test_MS()
